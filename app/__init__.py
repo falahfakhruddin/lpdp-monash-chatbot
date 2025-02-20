2c7c4ca3d9fa57e245a6d679271b7c4d30d411d3
Falah Fakhruddin <falahfakhruddin@gmail.com>
2025-02-20 22:38:44 +0700

    adjust create app flow

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
 
+from openai import OpenAI
+import utils.markdown as md
+import utils.json as uj
+import os
 
-def create_app(client, handbook_info, models):
+
+def create_app():
+    open_ai_key = os.getenv('OPENAI_API_KEY')
+    client = OpenAI(api_key=open_ai_key)
+
+    handbook_info = md.retrieve_data('handbook_extracted.md')
+    models = uj.load_json('embedding_model.json')
+
     app = Flask(__name__)
     CORS(app)  # Enable CORS for all routes
 
--- run.py
+++ run.py
@@ -5,12 +5,7 @@ from openai import OpenAI
 import utils.markdown as md
 import utils.json as uj
 
-open_ai_key = os.getenv('OPENAI_API_KEY')
-client = OpenAI(api_key=open_ai_key)
-
-handbook_info = md.retrieve_data('handbook_extracted.md')
-models = uj.load_json('embedding_model.json')
-app = create_app(client, handbook_info, models)
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
